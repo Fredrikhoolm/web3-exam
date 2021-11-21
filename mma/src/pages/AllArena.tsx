ae4f9878baf923944149a4668aaac8342f0b53ce
Johbra96 <61549336+Johbra96@users.noreply.github.com>
2021-11-21 23:58:39 +0100

    added allArena style (#7)

--- mma/src/pages/AllArena.tsx
+++ mma/src/pages/AllArena.tsx
@@ -1,22 +1,33 @@
 import { FC } from "react";
 import ArenaList from "../components/arena/ArenaList";
 import { ArenaProvider } from "../contexts/ArenaContext";
-import { Link } from "react-router-dom"
-import { Button } from "react-bootstrap"
-
+import { Link } from "react-router-dom";
+import { Button, Col, Row } from "react-bootstrap";
 
 const AllArena: FC = () => {
   return (
     <section>
-      <h3>Alle Arenaer</h3>
-      <div className="container">
-      <div>
-      <Link to={`/create-arena`}><Button variant="primary" > Legg til Arena </Button></Link>
-      </div>
-      </div>
+      <h3 className="text-center mt-5 mb-5 fs-1">Alle Arenaer</h3>
+      <div className="container"></div>
       <ArenaProvider>
         <ArenaList />
       </ArenaProvider>
+
+      <Row>
+        <Col></Col>
+        <Col>
+          <Link
+            className="d-grid gap-2 text-decoration-none mt-5 mb-5"
+            to={`/create-arena`}
+          >
+            <Button variant="outline-dark" size="lg">
+              {" "}
+              Legg til Arena{" "}
+            </Button>
+          </Link>
+        </Col>
+        <Col></Col>
+      </Row>
     </section>
   );
 };
