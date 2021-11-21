2fee2ecf1a6da8152c51285165904c1da554b697
Johbra96 <61549336+Johbra96@users.noreply.github.com>
2021-11-21 17:07:17 +0100

    Merge Frontend CreateArenaForm and CreateAthleteForm into master (#3)
    * finnished frontend lacking image
    
    * push fix

--- mma/src/components/arena/CreateNewArenaForm.tsx
+++ mma/src/components/arena/CreateNewArenaForm.tsx
@@ -1,53 +1,91 @@
 import { FC, ChangeEvent, useState } from "react";
 import { arenaService } from "../../services/arenaService";
 import { IArena } from "../../interfaces/IArena";
+import { Row, Form, Container, Col } from "react-bootstrap";
+import "../../css/CreateNewForm.css";
 
-const CreateNewArenaForm : FC = () => {   
+const CreateNewArenaForm: FC = () => {
+  const [newArena, setNewArena] = useState<IArena>({
+    name: "",
+    image: "",
+    capacity: "",
+  });
+  const [newImage, setNewImage] = useState<File>();
 
-    const [ newArena, setNewArena ] = useState<IArena>({name: "", image: "", capacity: ""});
-    const [ newImage, setNewImage ] = useState<File>();
-
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        let { name } = event.target;       
-        switch( name ){
-            case "name":
-                let { value } = event.target;
-                setNewArena( { ...newArena, name: value } );
-                break;
-            case "image":
-                let { files } = event.target;
-                if( files ){
-                    console.log( files[0] );
-                    setNewArena( { ...newArena, image: files[0].name } );
-                    setNewImage( files[0] );
-                }
-            break;
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let { name } = event.target;
+    let { value } = event.target;
+    switch (name) {
+      case "name":
+        setNewArena({ ...newArena, name: value });
+        break;
+      case "image":
+        let { files } = event.target;
+        if (files) {
+          console.log(files[0]);
+          setNewArena({ ...newArena, image: files[0].name });
+          setNewImage(files[0]);
         }
+        break;
+      case "capacity":
+        setNewArena({ ...newArena, capacity: value });
+        break;
     }
-    const postNewArena = () => {
-        console.log( newArena );
-        console.log( newImage ); 
-        arenaService.postNewArena(newArena, newImage as File);
-    }
+  };
+  const postNewArena = () => {
+    console.log(newArena);
+    console.log(newImage);
+    arenaService.postNewArena(newArena, newImage as File);
+  };
 
-    
-    return (
-        <section>
-            <div>
-                <label>Navn</label>
-                <input onChange={handleChange} name="name" type="text"/>
-            </div>
-            <div>
-                <label>Kapasitet</label>
-                <input onChange={handleChange} name="capacity" type="text"/>
-            </div>
-            <div>
-                <label>Velg bilde</label>
-                <input onChange={handleChange} name="image" type="file"/>
-            </div>
-            <input onClick={postNewArena} type="button" value="Lagre ny arena"/>
-        </section>
-    )
-}
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+            <Row>
+              {" "}
+              <Form.Group className="mb-3">
+                <Form.Label>Navn</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="name"
+                  type="text"
+                  placeholder="Skriv inn navn..."
+                />
+              </Form.Group>
+            </Row>
+
+            <Row>
+              <Form.Group className="mb-3">
+                <Form.Label>Kapasitet</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="capacity"
+                  type="number"
+                  placeholder="Skriv inn kapasitet..."
+                />
+              </Form.Group>
+            </Row>
+            <input
+              onClick={postNewArena}
+              type="button"
+              value="Lagre ny arena"
+            />
+          </Form>
+        </Col>
+        <Col>
+          <Container fluid className="ImgPlaceholderArena">
+            Placeholder Image
+          </Container>
+          <Form.Group>
+            <Form.Label>Velg bilde</Form.Label>
+            <Form.Control onChange={handleChange} name="image" type="file" />
+          </Form.Group>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default CreateNewArenaForm;
